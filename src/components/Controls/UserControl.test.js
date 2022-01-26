3351b292e38ee83dbb7e68b17760021ad243eea7
mira-kine <mirak0419@gmail.com>
2022-01-25 16:28:20 -0800

    fixing imports

--- src/components/Controls/UserControl.test.js
+++ src/components/Controls/UserControl.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { userEvent } from '@testing-library/user-event';
 import Pokemon from '../../views/Pokemon';
 
 test('pokemon should search by query', async () => {
