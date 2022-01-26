9a6787d20ed9656203f1e88fbf89745e54a9ef33
mira-kine <mirak0419@gmail.com>
2022-01-25 17:02:34 -0800

    npm

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test.skip('renders learn react link', () => {
+test('renders app', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = await screen.findByText(/search:/i);
   expect(linkElement).toBeInTheDocument();
 });
