a762b0d80e3784cff0b45bc3c80d8b3bece2c234
mira-kine <mirak0419@gmail.com>
2022-01-28 10:22:20 -0800

    cleaning up my code

--- src/App.test.js
+++ src/App.test.js
@@ -62,20 +62,14 @@ afterAll(() => server.close());
 
 test('pokemon should search by query', async () => {
   render(<App />);
-  // search textbox by role, userEvent typing name
   const searchBar = await screen.findByRole('textbox');
   const pokeName = 'butterfree';
   userEvent.type(searchBar, pokeName);
-  // Search butterfree with type then userEvent click button
   const button = screen.getByRole('button');
   userEvent.click(button);
-  // check to see that name on screen matches name in search bar, exact false
   const poke = await screen.findAllByText(pokeName, { exact: false });
-  // map through pokemon for correct text content = result of search
   const result = poke.map((item) => item.textContent);
-  // create function that will check if searchbar name = result name
   const handleName = (name) => name.toLowerCase().includes(pokeName);
-  // use .every to check if name fulfills handleName
   const checkName = result.every(handleName);
   expect(checkName).toBe(true);
 });
