7f5595238add98450c8597afd8c4d82126ae7759
mira-kine <mirak0419@gmail.com>
2022-01-27 17:58:08 -0800

    working on second msw test for behavioral

--- src/App.test.js
+++ src/App.test.js
@@ -676,11 +676,10 @@ import App from './App';
 //     },
 //   ],
 // };
-
 // const server = setupServer(
 //   rest.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex`, (req, res, ctx) => {
-//     const select = req.url.searchParams.get('select');
-//     if (select === '*') {
+//     const select = req.url.searchParams.get('pokemon');
+//     if (select === 'butterfree') {
 //       return res(ctx.json(pokemon));
 //     }
 //     return res(ctx.status(500), ctx.json({ error: 'failed' }));
@@ -691,63 +690,6 @@ import App from './App';
 // afterAll(() => server.close());
 
 test('pokemon should search by query', async () => {
-  // const butterfree = {
-  //   count: 1,
-  //   page: 1,
-  //   perPage: 20,
-  //   sort: {
-  //     by: '_id',
-  //     direction: 'asc',
-  //   },
-  //   search: {
-  //     pokemon: 'butter',
-  //   },
-  //   results: [
-  //     {
-  //       _id: '5ff4fb7cd89993a89cc6544f',
-  //       pokemon: 'butterfree',
-  //       id: 16,
-  //       species_id: 12,
-  //       height: 11,
-  //       weight: 320,
-  //       base_experience: 178,
-  //       type_1: 'bug',
-  //       type_2: 'flying',
-  //       attack: 45,
-  //       defense: 50,
-  //       hp: 60,
-  //       special_attack: 90,
-  //       special_defense: 80,
-  //       speed: 70,
-  //       ability_1: 'compound-eyes',
-  //       ability_2: 'NA',
-  //       ability_hidden: 'tinted-lens',
-  //       color_1: '#A8B820',
-  //       color_2: '#A890F0',
-  //       color_f: '#A8AE52',
-  //       egg_group_1: 'bug',
-  //       egg_group_2: 'NA',
-  //       url_image: 'http://assets.pokemon.com/assets/cms2/img/pokedex/full/012.png',
-  //       generation_id: 1,
-  //       evolves_from_species_id: '11',
-  //       evolution_chain_id: 4,
-  //       shape_id: 13,
-  //       shape: 'bug-wings',
-  //       pokebase: 'butterfree',
-  //       pokedex: 'http://www.pokemon.com/us/pokedex/butterfree',
-  //     },
-  //   ],
-  // };
-
-  // server.use(
-  //   rest.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex`, (req, res, ctx) => {
-  //     const select = req.url.searchParams.get('select');
-  //     if (select === '*') {
-  //       return res(ctx.json(butterfree));
-  //     }
-  //     return res(ctx.status(500), ctx.json({ error: 'failed' }));
-  //   })
-  // );
   render(<App />);
   // loading
   // const loading = await screen.getByText(/loading/i);
