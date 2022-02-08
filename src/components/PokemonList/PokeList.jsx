9fda8322d8b5edec75129c7a1ef1d85b46a7b8e7
mira-kine <mirak0419@gmail.com>
2022-02-08 14:27:39 -0800

    custom hooks

--- src/components/PokemonList/PokeList.jsx
+++ src/components/PokemonList/PokeList.jsx
@@ -3,17 +3,19 @@ import './PokeList.css';
 
 export default function PokeList({ poke }) {
   return (
-    <div>
+    <>
       <div className="poke-container">
         <ul>
           {poke.map((item) => (
             <div key={item.id} className="poke-card">
-              <li>{item.pokemon}</li>
+              <li className="name">{item.pokemon}</li>
+              <li>type: {item.type_1}</li>
+              <li>hp: {item.hp}</li>
               <img className="image" src={item.url_image} />
             </div>
           ))}
         </ul>
       </div>
-    </div>
+    </>
   );
 }
--- src/views/Pokemon.jsx
+++ src/views/Pokemon.jsx
@@ -3,11 +3,14 @@ import { useState } from 'react';
 import UserControl from '../components/Controls/UserControl';
 import { fetchPokemon } from '../services/pokemon';
 import PokeList from '../components/PokemonList/PokeList';
+import './Pokemon.css';
+import { useHover } from 'react-recipes';
 
 export default function Pokemon() {
   const [poke, setPoke] = useState([]);
   const [query, setQuery] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [hoverRef, isHovered] = useHover();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,7 +26,10 @@ export default function Pokemon() {
   return (
     <div>
       <h1>Pokedex</h1>
-      <UserControl setLoading={setLoading} query={query} setQuery={setQuery} />
+      <div ref={hoverRef}>
+        {isHovered ? '🤩 ' : '😗'}
+        <UserControl setLoading={setLoading} query={query} setQuery={setQuery} />
+      </div>
       <PokeList poke={poke} />
     </div>
   );
